5f60305d77da24e85628a2618e8a62d3b0567f54
mattfowler <matt.fow@gmail.com>
2020-12-27 08:03:58 -0500

    Adding listing 9.2

---
+++ chapter_09/listing_9_2.py
@@ -0,0 +1,46 @@
+import asyncpg
+from aiohttp import web
+from aiohttp.web_app import Application
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+from asyncpg import Record
+from asyncpg.pool import Pool
+from typing import List, Dict
+
+routes = web.RouteTableDef()
+DB_KEY = 'database'
+
+
+async def create_database_pool(app: Application): #A
+    print('Creating database pool.')
+    pool: Pool = await asyncpg.create_pool(host='0.0.0.0',
+                                           port=5432,
+                                           user='postgres',
+                                           password='password',
+                                           database='products',
+                                           min_size=6,
+                                           max_size=6)
+    app[DB_KEY] = pool
+
+
+async def destroy_database_pool(app: Application): #B
+    print('Destroying database pool.')
+    pool: Pool = app[DB_KEY]
+    await pool.close()
+
+
+@routes.get('/brands')
+async def brands(request: Request) -> Response: #C
+    connection: Pool = request.app[DB_KEY]
+    brand_query = 'SELECT brand_id, brand_name FROM brand'
+    results: List[Record] = await connection.fetch(brand_query)
+    result_as_dict: List[Dict] = [dict(brand) for brand in results]
+    return web.json_response(result_as_dict)
+
+
+app = web.Application()
+app.on_startup.append(create_database_pool) #D
+app.on_cleanup.append(destroy_database_pool)
+
+app.add_routes(routes)
+web.run_app(app)
