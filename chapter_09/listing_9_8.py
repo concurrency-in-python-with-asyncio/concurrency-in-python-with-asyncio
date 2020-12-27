96466be0a6e72cf088781fe269bfc50db2dc05cd
mattfowler <matt.fow@gmail.com>
2020-12-27 08:45:56 -0500

    Adding listing 9.8

---
+++ chapter_09/listing_9_8.py
@@ -0,0 +1,37 @@
+import asyncpg
+from asyncpg import Record
+from asyncpg.pool import Pool
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
+from starlette.routing import Route
+from typing import List, Dict
+
+
+async def create_database_pool():
+    pool: Pool = await asyncpg.create_pool(host='0.0.0.0',
+                                           port=5432,
+                                           user='postgres',
+                                           password='password',
+                                           database='products',
+                                           min_size=6,
+                                           max_size=6)
+    app.state.DB = pool
+
+
+async def destroy_database_pool():
+    pool = app.state.DB
+    await pool.close()
+
+
+async def brands(request: Request) -> Response:
+    connection: Pool = request.app.state.DB
+    brand_query = 'SELECT brand_id, brand_name FROM brand'
+    results: List[Record] = await connection.fetch(brand_query)
+    result_as_dict: List[Dict] = [dict(brand) for brand in results]
+    return JSONResponse(result_as_dict)
+
+
+app = Starlette(routes=[Route('/brands', brands)],
+                on_startup=[create_database_pool],
+                on_shutdown=[destroy_database_pool])
