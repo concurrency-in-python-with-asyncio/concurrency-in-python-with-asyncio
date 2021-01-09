b6fa735883b0ed2165631ed9077bacc0c3b66bb4
mattfowler <matt.fow@gmail.com>
2021-01-09 10:26:33 -0500

    Add listing 8.8

---
+++ chapter_08/listing_8_8.py
@@ -0,0 +1,24 @@
+import sys
+from asyncio import StreamReader
+from collections import deque
+from chapter_08.listing_8_7 import move_back_one_char, clear_line
+
+
+async def read_line(stdin_reader: StreamReader) -> str:
+    def erase_last_char():
+        move_back_one_char()
+        sys.stdout.write(' ')
+        move_back_one_char()
+
+    delete_char = b'\x7f'
+    input_buffer = deque()
+    while (input_char := await stdin_reader.read(1)) != b'\n':
+        if input_char == delete_char:
+            if len(input_buffer) > 0:
+                input_buffer.pop()
+                erase_last_char()
+        else:
+            input_buffer.append(input_char)
+            sys.stdout.write(input_char.decode())
+    clear_line()
+    return b''.join(input_buffer).decode()
