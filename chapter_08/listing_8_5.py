deaa7ca414dfd9ed8e1a58671cc6b5b6d2735127
mattfowler <matt.fow@gmail.com>
2021-01-09 10:16:37 -0500

    Add listing 8.5

---
+++ chapter_08/listing_8_5.py
@@ -0,0 +1,11 @@
+import asyncio
+import sys
+from asyncio import StreamReader
+
+
+async def create_stdin_reader() -> StreamReader:
+    stream_reader = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(stream_reader)
+    loop = asyncio.get_running_loop()
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+    return stream_reader
