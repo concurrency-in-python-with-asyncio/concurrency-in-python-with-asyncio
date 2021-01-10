bfd368fe030780d64d0f90b281b75f2797510ed2
mattfowler <matt.fow@gmail.com>
2021-01-10 08:11:10 -0500

    Check if the socket is still in the list before removing.

--- chapter_09/listing_9_9.py
+++ chapter_09/listing_9_9.py
@@ -8,19 +8,19 @@ class UserCounter(WebSocketEndpoint):
     encoding = 'text'
     sockets = []
 
-    async def on_connect(self, websocket): #A
+    async def on_connect(self, websocket):  # A
         await websocket.accept()
         UserCounter.sockets.append(websocket)
         await self._send_count()
 
-    async def on_disconnect(self, websocket, close_code): #B
+    async def on_disconnect(self, websocket, close_code):  # B
         UserCounter.sockets.remove(websocket)
         await self._send_count()
 
     async def on_receive(self, websocket, data):
         pass
 
-    async def _send_count(self): #C
+    async def _send_count(self):  # C
         if len(UserCounter.sockets) > 0:
             count_str = str(len(UserCounter.sockets))
             task_to_socket = {asyncio.create_task(websocket.send_text(count_str)): websocket
@@ -31,7 +31,8 @@ class UserCounter(WebSocketEndpoint):
 
             for task in done:
                 if task.exception() is not None:
-                    UserCounter.sockets.remove(task_to_socket[task])
+                    if task_to_socket[task] in UserCounter.sockets:
+                        UserCounter.sockets.remove(task_to_socket[task])
 
 
 app = Starlette(routes=[WebSocketRoute('/counter', UserCounter)])
