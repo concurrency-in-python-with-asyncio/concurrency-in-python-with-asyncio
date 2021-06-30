41c9d38ea6d10690e82c6640806113ace9fad798
mattfowler <matt.fow@gmail.com>
2021-06-30 17:27:34 -0400

    Fix spacing

--- chapter_06/listing_6_2.py
+++ chapter_06/listing_6_2.py
@@ -1,11 +1,13 @@
 from multiprocessing import Pool
 
+
 def say_hello(name: str) -> str:
     return f'Hi there, {name}'
 
+
 if __name__ == "__main__":
-    with Pool() as process_pool: #A
-        hi_jeff = process_pool.apply(say_hello, args=('Jeff',)) #B
+    with Pool() as process_pool:  # A
+        hi_jeff = process_pool.apply(say_hello, args=('Jeff',))  # B
         hi_john = process_pool.apply(say_hello, args=('John',))
         print(hi_jeff)
         print(hi_john)
