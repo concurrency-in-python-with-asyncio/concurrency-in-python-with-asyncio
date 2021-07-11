2ab3e92f968978146deff2db559bb14dac18f2ff
mattfowler <matt.fow@gmail.com>
2021-07-11 10:45:55 -0400

    Use a shared queue to better show the concept

--- chapter_07/listing_7_14.py
+++ chapter_07/listing_7_14.py
@@ -1,16 +1,19 @@
+from queue import Queue, Empty
 from tkinter import Tk
 from tkinter import Label
 from tkinter import Entry
 from tkinter import ttk
 from typing import Optional
-
 from chapter_07.listing_7_13 import StressTest
 
 
 class LoadTester(Tk):
 
-    def __init__(self, loop, *args, **kwargs): #A
+    def __init__(self, loop, *args, **kwargs):  # A
         Tk.__init__(self, *args, **kwargs)
+        self._queue = Queue()
+        self._refresh_ms = 25
+
         self._loop = loop
         self._load_test: Optional[StressTest] = None
         self.title('URL Requester')
@@ -27,7 +30,7 @@ class LoadTester(Tk):
         self._request_field = Entry(self, width=10)
         self._request_field.grid(column=1, row=1)
 
-        self._submit = ttk.Button(self, text="Submit", command=self._start) #B
+        self._submit = ttk.Button(self, text="Submit", command=self._start)  # B
         self._submit.grid(column=2, row=1)
 
         self._pb_label = Label(self, text="Progress:")
@@ -36,23 +39,33 @@ class LoadTester(Tk):
         self._pb = ttk.Progressbar(self, orient="horizontal", length=200, mode="determinate")
         self._pb.grid(column=1, row=3, columnspan=2)
 
-    def _update_bar(self, pct: int): #C
+    def _update_bar(self, pct: int):  # C
         if pct == 100:
             self._load_test = None
             self._submit['text'] = 'Submit'
+        else:
+            self._pb['value'] = pct
+            self.after(self._refresh_ms, self._poll_queue)
 
-        self._pb['value'] = pct
+    def _queue_update(self, completed_requests: int, total_requests: int):  # D
+        self._queue.put(int(completed_requests / total_requests * 100))
 
-    def _trigger_update(self, completed_requests: int, total_requests: int): #D
-        self.after_idle(self._update_bar, int((completed_requests / total_requests) * 100))
+    def _poll_queue(self): # E
+        try:
+            percent_complete = self._queue.get_nowait()
+            self._update_bar(percent_complete)
+        except Empty:
+            if self._load_test:
+                self.after(self._refresh_ms, self._poll_queue)
 
-    def _start(self): #E
+    def _start(self):  # F
         if self._load_test is None:
             self._submit['text'] = 'Cancel'
             test = StressTest(self._loop,
                               self._url_field.get(),
                               int(self._request_field.get()),
-                              self._trigger_update)
+                              self._queue_update)
+            self.after(self._refresh_ms, self._poll_queue)
             test.start()
             self._load_test = test
         else:
