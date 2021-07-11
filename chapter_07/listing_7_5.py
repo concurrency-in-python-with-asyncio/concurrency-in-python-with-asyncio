f3d1ab55d9ef72772fe661f59f0cf4b26fa104db
mattfowler <matt.fow@gmail.com>
2021-07-11 09:37:54 -0400

    Fix incorrect parameters

--- chapter_07/listing_7_5.py
+++ chapter_07/listing_7_5.py
@@ -18,7 +18,7 @@ async def main():
     loop = asyncio.get_running_loop()
     with ThreadPoolExecutor() as pool:
         urls = ['https://www.example.com' for _ in range(1000)]
-        tasks = [loop.run_in_executor(pool, functools.partial(get_status_code, url, loop)) for url in urls]
+        tasks = [loop.run_in_executor(pool, functools.partial(get_status_code, url)) for url in urls]
         results = await asyncio.gather(*tasks)
         print(results)
 
