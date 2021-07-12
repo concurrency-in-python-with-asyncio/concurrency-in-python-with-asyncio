cc81a01c32168b39de0342e3482944cd8888768e
mattfowler <matt.fow@gmail.com>
2021-07-11 20:15:04 -0400

    Use https

--- chapter_04/listing_4_2.py
+++ chapter_04/listing_4_2.py
@@ -13,7 +13,7 @@ async def fetch_status(session: ClientSession, url: str) -> int:
 @async_timed()
 async def main():
     async with aiohttp.ClientSession() as session:
-        url = 'http://www.example.com'
+        url = 'https://www.example.com'
         status = await fetch_status(session, url)
         print(f'Status for {url} was {status}')
 
