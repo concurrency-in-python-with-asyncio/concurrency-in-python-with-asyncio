46a54bcac77600780ea02d281c523db858dd171f
mattfowler <matt.fow@gmail.com>
2021-07-11 20:10:50 -0400

    Use an instance variable

--- chapter_04/listing_4_1.py
+++ chapter_04/listing_4_1.py
@@ -5,15 +5,15 @@ from typing import Optional, Type
 
 
 class ConnectedSocket:
-    _connection = None
 
     def __init__(self, server_socket):
-        self.server_socket = server_socket
+        self._connection = None
+        self._server_socket = server_socket
 
     async def __aenter__(self):
         print('Entering context manager, waiting for connection')
         loop = asyncio.get_event_loop()
-        connection, address = await loop.sock_accept(self.server_socket)
+        connection, address = await loop.sock_accept(self._server_socket)
         self._connection = connection
         print('Accepted a connection')
         return self._connection
@@ -31,6 +31,7 @@ async def main():
     loop = asyncio.get_event_loop()
 
     server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_address = ('127.0.0.1', 8000)
     server_socket.setblocking(False)
     server_socket.bind(server_address)
