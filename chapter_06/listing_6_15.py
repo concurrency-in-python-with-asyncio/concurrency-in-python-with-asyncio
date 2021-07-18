e1b3d5588c9b77442cb0d7f9a280adcf8426199a
mattfowler <matt.fow@gmail.com>
2021-07-18 10:49:58 -0400

    Add product query

--- chapter_06/listing_6_15.py
+++ chapter_06/listing_6_15.py
@@ -3,7 +3,22 @@ import asyncpg
 from util import async_timed
 from typing import List, Dict
 from concurrent.futures.process import ProcessPoolExecutor
-from chapter_05.listing_5_7 import product_query
+
+
+product_query = \
+    """
+SELECT
+p.product_id,
+p.product_name,
+p.brand_id,
+s.sku_id,
+pc.product_color_name,
+ps.product_size_name
+FROM product as p
+JOIN sku as s on s.product_id = p.product_id
+JOIN product_color as pc on pc.product_color_id = s.product_color_id
+JOIN product_size as ps on ps.product_size_id = s.product_size_id
+WHERE p.product_id = 100"""
 
 
 async def query_product(pool):
