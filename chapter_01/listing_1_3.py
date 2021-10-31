11fcb75202549fcfeeae0a5924b81fa1ef8acad4
mattfowler <matt.fow@gmail.com>
2021-10-31 09:56:55 -0400

    Use property instead of method for thread name

--- chapter_01/listing_1_3.py
+++ chapter_01/listing_1_3.py
@@ -9,7 +9,7 @@ hello_thread = threading.Thread(target=hello_from_thread)
 hello_thread.start()
 
 total_threads = threading.active_count()
-thread_name = threading.current_thread().getName()
+thread_name = threading.current_thread().name
 
 print(f'Python is currently running {total_threads} thread(s)')
 print(f'The current thread is {thread_name}')
