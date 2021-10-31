15b5ad11b6d1f105a0fbca5e6fd2e6c75de58e68
mattfowler <matt.fow@gmail.com>
2021-10-31 10:19:41 -0400

    Remove one print so diagram will fit on page.

--- chapter_02/listing_2_10.py
+++ chapter_02/listing_2_10.py
@@ -3,14 +3,14 @@ from util import delay
 
 
 async def hello_every_second():
-    for i in range(3):
+    for i in range(2):
         await asyncio.sleep(1)
         print("I'm running other code while I'm waiting!")
 
 
 async def main():
-    first_delay = asyncio.create_task(delay(4))
-    second_delay = asyncio.create_task(delay(4))
+    first_delay = asyncio.create_task(delay(3))
+    second_delay = asyncio.create_task(delay(3))
     await hello_every_second()
     await first_delay
     await second_delay
