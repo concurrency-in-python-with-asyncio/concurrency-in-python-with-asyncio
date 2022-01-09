b839fee90768ea86cd967a62da57cd0bfff4f364
mattfowler <matt.fow@gmail.com>
2022-01-09 09:02:04 -0500

    Formatting fix

--- chapter_04/listing_4_10.py
+++ chapter_04/listing_4_10.py
@@ -7,8 +7,9 @@ from chapter_04 import fetch_status
 @async_timed()
 async def main():
     async with aiohttp.ClientSession() as session:
-        fetchers = [asyncio.create_task(fetch_status(session, 'https://example.com')),
-                    asyncio.create_task(fetch_status(session, 'https://example.com'))]
+        fetchers = \
+          [asyncio.create_task(fetch_status(session, 'https://example.com')),
+           asyncio.create_task(fetch_status(session, 'https://example.com'))]
         done, pending = await asyncio.wait(fetchers)
 
         print(f'Done task count: {len(done)}')
